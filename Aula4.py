8fab01da88852a36c4bb7983d3c30a27df531963
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-18 22:49:35 -0300

    Testar se os faturamentos são iguais

---
+++ Aula4.py
@@ -0,0 +1,11 @@
+#Aula 4
+faturamento_loja_um = int(input('Entre com o faturamento da loja 1: '))
+faturamento_loja_dois = int(input('Entre com o faturamento da loja 2: '))
+email = "leonardogmail"
+
+print('Programa 1')
+if faturamento_loja_um == faturamento_loja_dois:
+    print('Os faturamentos são iguais. ')
+else:
+    print('Os faturamentos são diferentes')
+
